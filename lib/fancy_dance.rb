e7fb73b43e9219d6666bfaf2a146d80b809d6d8b
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-02 01:05:17 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -23,7 +23,7 @@ module FancyDance
   module ClassMethods
 
     def metadata
-      "This class produces objects that love to dance"
+      "This class produces objects that love to dance."
     end
   end
 end
