9ed252fdd7433a7c3856b44fbb9b4339fee5d997
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-02 01:07:46 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -13,8 +13,7 @@ module Dance
   end
 
   def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you
-    all."
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 
 end
